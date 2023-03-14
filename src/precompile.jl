9f26c2e753b3d58b3ede0bba208786cc696bc0cd
tylerjthomas9 <tyler@saguarocm.com>
2023-03-14 10:37:21 -0700

    revert some precompile

--- src/precompile.jl
+++ src/precompile.jl
@@ -18,30 +18,30 @@ using SnoopPrecompile
         assets = [Equity(:AMD), Equity(:INTC), Equity(:NVDA)]
         universe = StaticUniverse(assets)
         # This brings the CSVDailyBarSource test from 5s to 0.3s on v1.9-rc1
-        ds = CSVDailyBarSource("./test/test_data") #TODO: should we just hardcode an example df?
-        dh = BacktestDataHandler([ds])
-        port_optimizer = EqualWeightPortfolioOptimizer(dh)
-        broker = SimulatedBroker(
-            DateTime(2020), SimulatedExchange(DateTime(2020)), dh; initial_cash=100_000.0
-        )
-        create_portfolio!(broker, 75_000; portfolio_id=portfolio_id)
-        order_sizer = DollarWeightedOrderSizer(0.01)
+        # ds = CSVDailyBarSource("./test/test_data") #TODO: should we just hardcode an example df?
+        # dh = BacktestDataHandler([ds])
+        # port_optimizer = EqualWeightPortfolioOptimizer(dh)
+        # broker = SimulatedBroker(
+        #     DateTime(2020), SimulatedExchange(DateTime(2020)), dh; initial_cash=100_000.0
+        # )
+        # create_portfolio!(broker, 75_000; portfolio_id=portfolio_id)
+        # order_sizer = DollarWeightedOrderSizer(0.01)
         signal_weights = Dict(Equity(:AMD) => 1.0, Equity(:INTC) => 1.0)
         alpha_model = FixedSignalsAlphaModel(signal_weights)
-        port_optimizer = FixedWeightPortfolioOptimizer(dh)
+        # port_optimizer = FixedWeightPortfolioOptimizer(dh)
         rebalance = DailyRebalance(Date(start_dt), Date(end_dt))
 
-        trading_session = BacktestTradingSession(
-            start_dt,
-            end_dt,
-            universe,
-            broker,
-            alpha_model,
-            rebalance,
-            portfolio_id,
-            order_sizer,
-            port_optimizer,
-        )
-        run!(trading_session)
+        # trading_session = BacktestTradingSession(
+        #     start_dt,
+        #     end_dt,
+        #     universe,
+        #     broker,
+        #     alpha_model,
+        #     rebalance,
+        #     portfolio_id,
+        #     order_sizer,
+        #     port_optimizer,
+        # )
+        # run!(trading_session)
     end
 end
